a10b8601656771ae53760d9762497bda1f408c31
Vera <131263432+verounsen@users.noreply.github.com>
2025-08-17 15:17:26 +0200

    added router and first menu

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -3,24 +3,16 @@
 </script>
 
 <template>
-  <h1>Playground</h1>
-  <div class="layout">
-      <img src="../public/layout/playground.png" class="layout-img">
+  <h1>Playground</h1> 
+  <div>
+    <img src="/layout/playground.png">
   </div>
 </template>
 
 <style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-  left: 0;
-  top: 0;
-}
-
-.layout-img {
-  scale:0.35;
-  transform-origin: top left; 
-  opacity: 0; //TODO
-  //border: 3px solid red;
+img{
+  transform-origin: top left;
+  width: 900px;
+  height: auto;
 }
 </style>
